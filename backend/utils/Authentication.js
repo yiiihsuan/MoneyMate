641fbf9a13251d98cc1be1c454e3ac5f050e362b
yihsuan <yiiihsuann@gmail.com>
2023-12-06 01:31:29 +0800

    fix : authentication

--- backend/utils/Authentication.js
+++ backend/utils/Authentication.js
@@ -1,6 +1,49 @@
 import axios from 'axios';
 import 'dotenv/config';
 
+async function isAuthenticated(req, res, next) {
+    try {
+        const accessToken = req.cookies.accessToken;
+        if (!accessToken) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        const verifyResponse = await axios.get(`https://api.line.me/oauth2/v2.1/verify?access_token=${accessToken}`);
+        const userData = verifyResponse.data;
+
+        if (userData.client_id !== process.env.LINE_CLIENT_ID) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        const profileResponse = await axios.get('https://api.line.me/v2/profile', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        const userProfile = profileResponse.data;
+        const userId = userProfile.userId;
+        req.userId = userId;
+        console.log(`User ID: ${userId}`);
+        console.log(`req user: ${req.userId}`);
+        return next();
+    } catch (error) {
+        console.error('Error:', error);
+        return res.status(500).send('Internal Server Error'); // 更详细的错误处理
+    }
+}
+
+export default isAuthenticated;
+
+
+
+
+
+
+/*
+import axios from 'axios';
+import 'dotenv/config';
+
 function isAuthenticated(req, res, next) {
     console.log(req.cookies)
     const accessToken = req.cookies.accessToken;
@@ -12,8 +55,8 @@ function isAuthenticated(req, res, next) {
         axios.get(`https://api.line.me/oauth2/v2.1/verify?access_token=${req.cookies.accessToken}`)
             .then(response => {
                 if (response.data.client_id === process.env.LINE_CLIENT_ID) {
-                     console.log('userId in is authenticated', req.userId );
-                     req.userId = response.data.sub;
+                     
+                     
                     console.log('response data',response.data);
                     return next();
                 } else {
@@ -29,3 +72,4 @@ function isAuthenticated(req, res, next) {
 }
 
 export default isAuthenticated;
+*/
