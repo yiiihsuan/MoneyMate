fe6a3a00b648ccf539c2bbdb8be226f39632de78
yihsuan <yiiihsuann@gmail.com>
2023-12-08 17:00:19 +0800

    fix: save amount's time

--- backend/controllers/WebhookController.js
+++ backend/controllers/WebhookController.js
@@ -112,6 +112,7 @@ const handlePostback = async (event) => {
 
             let createdTime = new Date(record.created_time);
             let formattedTime = createdTime.toISOString().replace('T', ' ').substring(0, 19);
+            console.log('time while save',formattedTime );
 
 
             const confirmMessage = {
