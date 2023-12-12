b38915bff43cbdadf34d0bfac6dbceb50c31216b
yihsuan <yiiihsuann@gmail.com>
2023-12-12 20:00:44 +0800

    feat: try spinner

--- frontend/src/pages/AccountingBook.js
+++ frontend/src/pages/AccountingBook.js
@@ -69,6 +69,35 @@ const PieChartPlaceholder = styled.div`
 `;
 
 
+const Spinner = styled.div`
+  margin: 100px auto;
+  width: 40px;
+  height: 40px;
+  position: relative;
+
+  div {
+    background-color: #333;
+    width: 15px;
+    height: 15px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    animation: sk-cubemove 1.8s infinite ease-in-out;
+  }
+
+  .cube2 {
+    animation-delay: -0.9s;
+  }
+
+  @keyframes sk-cubemove {
+    25% { transform: translateX(42px) rotate(-90deg) scale(0.5); }
+    50% { transform: translateX(42px) translateY(42px) rotate(-180deg); }
+    75% { transform: translateX(0px) translateY(42px) rotate(-270deg) scale(0.5); }
+    100% { transform: rotate(-360deg); }
+  }
+`;
+
+
 
 const AccountingBook = () => {
   
@@ -200,7 +229,26 @@ const AccountingBook = () => {
   //   });
  
 
-  if (isLoading) return <div>Loading...</div>; // 加載狀態
+  //if (isLoading) return <div>Loading...</div>; // 加載狀態
+
+
+  if (isLoading) {
+    return (
+     
+ 
+        <Spinner>
+    <div className="cube1"></div>
+    <div className="cube2"></div>
+  </Spinner>
+  
+    
+    );
+  }
+
+
+
+
+
   if (isError) return <div>Error fetching data</div>; // 錯誤處理
 
 
